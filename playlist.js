f92378c7d57afb73166e91fc49a495af29bdeded
‘Michael <michael.daniels.jr@gmail.com>
2018-01-25 18:44:00 -0700

    project complete

--- playlist.js
+++ playlist.js
@@ -1,4 +1,14 @@
 let dataArray;
+let selectedArray = [];
+
+document.getElementById('clearTracks').addEventListener('click', function() {
+  document.getElementById('playlistTextArea').innerHTML = ""
+  selectedArray = [];
+})
+document.getElementById('submitBin').addEventListener('click', function() {
+  document.getElementById('playlistTextArea').innerHTML = ""
+  selectedArray = [];
+})
 
 fetch ('https://lit-fortress-6467.herokuapp.com/object').then(function(response2){
   return response2.json()
@@ -11,7 +21,6 @@ fetch ('https://lit-fortress-6467.herokuapp.com/object').then(function(response2
   for (let j = 0; j < dataLength2; j++) {
     let objectID = data2.results[j].id;
     let objectIdx = j;
-    console.log(objectID)
     let scrollingSinglePic = `
     <div class="scrolling-item"><img id="album${objectID}" class="scrolling-images" src="images/${data2.results[j].cover_art}" onclick="getImageInfo(${objectID}, ${objectIdx})" /></div>
     `
@@ -24,9 +33,37 @@ fetch ('https://lit-fortress-6467.herokuapp.com/object').then(function(response2
   new Error(err)
 });
 
+
+
 function getImageInfo(theID, objectIdx) {
-  console.log("getImageInfo",theID)
+
+  document.getElementById('submitBin').innerHTML = "submit bin";
+
   console.log(dataArray[objectIdx])
+  selectedArray.push(dataArray[objectIdx]);
+
+
   document.getElementById('playlistTextArea').innerHTML += `
-${dataArray[objectIdx].artist}: ${dataArray[objectIdx].artist}`
+${dataArray[objectIdx].artist}: ${dataArray[objectIdx].title}`
 }
+
+document.getElementById('submitBin').addEventListener('click', function() {
+
+  if (selectedArray !== []) {
+
+    console.log(selectedArray)
+
+    fetch('https://lit-fortress-6467.herokuapp.com/post', {
+      method: 'post',
+      body: JSON.stringify(selectedArray)
+    }).then(function(response) {
+      return response; // .json()
+    }).then(function(data) {
+      console.log(data);
+      document.getElementById('submitBin').innerHTML = "playlist submitted"
+    });
+
+  }
+
+
+})
